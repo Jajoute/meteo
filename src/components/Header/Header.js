a178d76fd7cbe917e56c5a879e161ad7af7d0a51
Jajoute <invictusrokenson@gmail.com>
2022-07-14 22:17:56 +0200

    add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => (
+  <div className="Header">
+    <a className="App-link" href="https://open-meteo.com">
+      API Open-meteo
+    </a>
+  </div>
+);
+
+export default Header;

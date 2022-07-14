3456c9d7095e17b88fa90b6bda5f3a8bd1114156
Jajoute <invictusrokenson@gmail.com>
2022-07-14 22:18:41 +0200

    add Weather component

---
+++ src/components/Weather/Weather.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import './Weather.scss';
+import logo from '../../logo.svg';
+import pressure from '../../asset/Group Thermometer.svg'
+import humidity from '../../asset/humidity.svg'
+import windspeed from '../../asset/windspeed.svg'
+import cloudy from '../../asset/WeatherIcon - 2-39.svg'
+import sun from '../../asset/WeatherIcon - 2-22.svg'
+import axios from 'axios';
+import moment from 'moment';
+import code from "./code";
+
+const Weather = () => {
+  const [weather, setWeather] = useState(null);
+
+  async function getWeather() {
+    try {
+      const response = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=48.8567&longitude=2.3510&hourly=temperature_2m,relativehumidity_2m,surface_pressure&current_weather=true');
+      setWeather(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function getIndex(weatherData){
+    let time = moment().format("YYYY-MM-DD").toString()+"T"+moment().format("HH").toString()+":00";
+    return weatherData.hourly.time.findIndex((value) => (value===time));
+  }
+  
+  function getHumidity(weatherData){
+    let humidity = weatherData.hourly.relativehumidity_2m[getIndex(weatherData)];
+    return humidity.toString();
+  }
+
+  function getPressure(weatherData){
+    let humidity = weatherData.hourly.surface_pressure[getIndex(weatherData)];
+    return humidity.toString();
+  }
+
+  function getWeatherTextCode(){
+    return code[weather.current_weather.weathercode];
+  }
+  
+  function getImage(weather){
+    if(weather.current_weather.weathercode === 0){
+      return sun;
+    }else{
+      return cloudy;
+    }
+  }
+  
+  useEffect(() => {
+    getWeather();
+  })
+  
+  return (
+  <div className="Weather">
+    <p className="Loc-temp">Paris</p>
+    <p className="Time-weather">{weather?"Latitude: "+weather.latitude+" Longitude: "+weather.longitude:""}</p>
+    <img src={weather?getImage(weather):logo} className="App-logo" alt="logo" />
+    <p className="Time-weather">{weather?getWeatherTextCode():""}</p>
+    <p className="Loc-temp">{weather?weather.current_weather.temperature+"°C":""}</p>
+
+    <div className="data-weather">
+      <div>
+        <img src={humidity} className="App-humidity" alt="humidity" />
+        <p>{weather?getHumidity(weather)+"%":""}</p>
+      </div>
+      <div>
+        <img src={windspeed} className="App-windspeed" alt="windspeed" />
+        <p>{weather?weather.current_weather.windspeed+"Km/h":""}</p>
+      </div>
+      <div>
+        <img src={pressure} className="App-pressure" alt="pressure" />
+        <p>{weather?getPressure(weather)+"hPa":""}</p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Weather;
---
+++ src/components/Weather/codeImage.js
@@ -0,0 +1,32 @@
+const codeImage = {
+  "0": "../../asset/WeatherIcon - 2-22.svg",
+  "1": "Principalement clair",
+  "2": "partiellement nuageux",
+  "3": "nuageux",
+  "45": "Brouillard",
+  "48": "Dépôt de brouillard givré",
+  "51": "Bruine : Intensité légère",
+  "53": "Bruine : modérée",
+  "55": "Bruine : dense",
+  "56":"Brouillard givrant : Intensité légère",
+  "57":"Brouillard givrant : dense",
+  "61":"Pluie : intensité légère",
+  "63":"Pluie : intensité modérée",
+  "65":"Pluie : intensité forte",
+  "66":"Pluie verglaçante : Intensité légère",
+  "67":"Pluie verglaçante : Intensité forte",
+  "71":"Chutes de neige : Intensité légère",
+  "73":"Chutes de neige : Intensité modérée",
+  "75":"Chutes de neige : Intensité forte",
+  "77":"Grains de neige",
+  "80":"Averses de pluie : Légères",
+  "81":"Averses de pluie : modérées",
+  "82":"Averses de pluie : violentes",
+  "85":"averses de neige légères",
+  "86":"averses de neige fortes",
+  "95 *":"Orage : Léger ou modéré",
+  "96 *":"Orage avec grêle légère",
+  "99 *":"Orage avec grêle forte"
+};
+
+export default codeImage;
